8cc8d5b7b2d611e3b3373691b8081689f3a4136f
Jose Perez FFC <joserph.a@gmail.com>
2020-09-04 16:05:02 -0500

    ass seed permission

--- database/seeds/PermissionInfoSeeder.php
+++ database/seeds/PermissionInfoSeeder.php
@@ -5,6 +5,7 @@ use App\User;
 use App\PermissionFolder\Models\Role;
 use App\PermissionFolder\Models\Permission;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class PermissionInfoSeeder extends Seeder
 {
@@ -15,6 +16,14 @@ class PermissionInfoSeeder extends Seeder
      */
     public function run()
     {
+        //Truncate
+        DB::statement("SET foreign_key_checks=0");
+            DB::table('role_user')->truncate();
+            DB::table('permission_role')->truncate();
+            Permission::truncate();
+            Role::truncate();
+        DB::statement("SET foreign_key_checks=1");
+
         // User Admin
         $userAdmin = User::where('email', 'admin@admin.com')->first();
         if($userAdmin)
@@ -26,6 +35,14 @@ class PermissionInfoSeeder extends Seeder
             'email'     => 'admin@admin.com',
             'password'  => Hash::make('admin')
         ]);
+
+        $rolAdmin = Role::create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'description' => 'Administrador',
+            'full-access' => 'yes'
+        ]);
         
+        $userAdmin->roles()->sync([$rolAdmin->id]);
     }
 }
