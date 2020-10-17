cf416498cbcdb3508101db569b8e633d480733f1
jose perez <joserph.a@gmail.com>
2020-10-16 20:53:39 -0500

    add card in show

--- resources/views/role/show.blade.php
+++ resources/views/role/show.blade.php
@@ -1,6 +1,61 @@
 @extends('layouts.principal')
 
 @section('content')
+<section class="content-header">
+    <div class="container-fluid">
+       <div class="row mb-2">
+          <div class="col-sm-6">
+             <h1>{{ $role->name}}
+                
+             </h1>
+          </div>
+          <div class="col-sm-6">
+             <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+             <li class="breadcrumb-item"><a href="{{ route('role.index') }}">Role(s)</a></li>
+             <li class="breadcrumb-item active">{{ $role->name}}</li>
+             </ol>
+          </div>
+       </div>
+    </div><!-- /.container-fluid -->
+ </section>
+
+
+ <section class="content">
+    <div class="container-fluid">
+       <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+            <div class="card-header">
+                
+                <i class="fas fa-text-width"></i>
+                Detalle del role
+                
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <dl class="row">
+                <dt class="col-sm-4">Nombre</dt>
+                <dd class="col-sm-8">{{ $role->name }}</dd>
+                <dt class="col-sm-4">Slug</dt>
+                <dd class="col-sm-8">{{ $role->slug }}</dd>
+                <dt class="col-sm-4">Descripcción</dt>
+                <dd class="col-sm-8">{{ $role->description }}</dd>
+                <dt class="col-sm-4">Full acceso</dt>
+                <dd class="col-sm-8">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
+                    sit amet risus.
+                </dd>
+                </dl>
+            </div>
+            <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+       </div>
+    </div>
+ </section>
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
